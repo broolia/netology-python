b8705db522b41268b6823ef60413e4f71fb84bc7
Yulia Brueva <yuliabrueva@gmail.com>
2024-10-18 11:35:24 +0200

    add new lessons

---
+++ sort-names.py
@@ -0,0 +1,31 @@
+def solve(boys: list, girls: list):
+    result = " " # в эту строку сохраните полученные пары или сообщение "Кто-то может остаться без пары!"
+    
+    if len(boys) == len(girls): # проверьте, что длины списков одинаковы
+        for boy, girl in zip(sorted(boys), sorted(girls)):
+            if len(result) > 0: # отсортируйте пары, объедините при помощи zip и распакуйте в цикле
+                result += f"{boy} и {girl}, "
+        result = result[:-2]
+    else:
+        result = "Кто-то может остаться без пары!"
+    return result
+
+if __name__ == '__main__':
+    # Этот код менять не нужно
+    boys = ['Peter', 'Alex', 'John', 'Arthur', 'Richard']
+    girls = ['Kate', 'Liza', 'Kira', 'Emma', 'Trisha']
+    result = solve(boys, girls)
+    assert result == "Alex и Emma, Arthur и Kate, John и Kira, Peter и Liza, Richard и Trisha", f"Знакомство не удалось: {result}"
+    print(f"Результат знакомства: {result}")
+
+    boys = ['Peter', 'Alex', 'John', 'Arthur']
+    girls = ['Kate', 'Liza', 'Kira', 'Emma', 'Trisha']
+    result = solve(boys, girls)
+    assert result == "Кто-то может остаться без пары!", f"Знакомство не удалось: {result}"
+    print(f"Результат знакомства: {result}")
+
+    boys = ['Peter', 'Alex', 'John', 'Arthur', 'Richard']
+    girls = ['Kate', 'Liza', 'Kira', 'Emma']
+    result = solve(boys, girls)
+    assert result == "Кто-то может остаться без пары!", f"Знакомство не удалось: {result}"
+    print(f"Результат знакомства: {result}")
